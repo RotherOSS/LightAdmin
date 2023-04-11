9c64d4190958ce991acf4017fc33997e4d6b5b68
Stefan Härter <sha@rother-oss.com>
2023-04-11 16:14:19 +0200

    Renaming sopm and language file.

--- Kernel/Language/de_LightAdmin.pm
+++ Kernel/Language/de_LightAdmin.pm
@@ -14,7 +14,7 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 # --
 
-package Kernel::Language::de_RotherOSSLightAdmin;
+package Kernel::Language::de_LightAdmin;
 
 use strict;
 use warnings;
